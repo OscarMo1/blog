3e6141f9818d2f369801fcf310646555f756b243
OscarMo1 <111909746+OscarMo1@users.noreply.github.com>
2023-08-15 15:02:27 -0400

    added tailwind and created header
    added tailwind css and created header with responsive hover features

---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import MainLayout from '../component/MainLayout'
+
+const Homepage = () => {
+  return (
+    <MainLayout>
+
+    </MainLayout>
+
+  )
+}
+
+export default Homepage
