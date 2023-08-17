cd389cdc72e430ed8f52a4de7a9146dc89269eb0
OscarMo1 <111909746+OscarMo1@users.noreply.github.com>
2023-08-17 13:51:27 -0400

    article card 2
    Added another article card and started on the CTA portion

--- src/constants/images.js
+++ src/constants/images.js
@@ -2,9 +2,10 @@ import Logo from '../assets/Logo.svg';
 import Hero from '../assets/HeroImage.svg';
 import Post1Image from '../assets/posts/post1.jpg';
 import Post1Profile from '../assets/posts/post-profile.svg';
+import Post2 from '../assets/posts/post.jpeg';
 
 const images = {
-    Logo, Hero, Post1Image, Post1Profile
+    Logo, Hero, Post1Image, Post1Profile, Post2
 };
 
 export default images
--- src/pages/Home/container/Article.jsx
+++ src/pages/Home/container/Article.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ArticleCard from '../../../component/ArticleCard';
+import ArticleCard1 from '../../../component/ArticleCard1';
 import {FaArrowRight} from 'react-icons/fa'
 
 
@@ -8,11 +9,11 @@ const Article = () => {
     <section className="flex flex-col container mx-auto px-5 py-10">
         <div className="flex flex-wrap md:gap-x-5 gap-y-5 pb-10">
             <ArticleCard className="w-full md:w-[calc(50%-20px)] lg:w-[calc(33.33%-21px)] "/>
-            <ArticleCard className="w-full md:w-[calc(50%-20px)] lg:w-[calc(33.33%-22px)]"/>
+            <ArticleCard1 className="w-full md:w-[calc(50%-20px)] lg:w-[calc(33.33%-22px)]"/>
         </div>
-        <button>
+        <button className="mx-auto flex items-center gap-x-2 font-bold text-primary border-2 border-primary px-6 py-3 rounded-lg">
             <span> More Articles</span>
-            <FaArrowRight/>
+            <FaArrowRight className="w-3 h-3"/>
         </button>
     </section>
   )
