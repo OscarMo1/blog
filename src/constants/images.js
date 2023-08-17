ef8ab7ed2a7f5135f47feaaf4bb8e4ee48dd7f30
OscarMo1 <111909746+OscarMo1@users.noreply.github.com>
2023-08-17 14:58:01 -0400

    CTA
    updated homepage with CTA

--- src/constants/images.js
+++ src/constants/images.js
@@ -3,9 +3,10 @@ import Hero from '../assets/HeroImage.svg';
 import Post1Image from '../assets/posts/post1.jpg';
 import Post1Profile from '../assets/posts/post-profile.svg';
 import Post2 from '../assets/posts/post.jpeg';
+import CtaImage from '../assets/CtaImage.jpg';
 
 const images = {
-    Logo, Hero, Post1Image, Post1Profile, Post2
+    Logo, Hero, Post1Image, Post1Profile, Post2, CtaImage
 };
 
 export default images
--- src/pages/Home/Homepage.jsx
+++ src/pages/Home/Homepage.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import MainLayout from '../../component/MainLayout';
 import Hero from './container/Hero';
 import Article from './container/Article';
+import CTA from './container/CTA';
 
 const Homepage = () => {
   return (
     <MainLayout>
       <Hero/>
       <Article/>
+      <CTA/>
     </MainLayout>
 
   )
