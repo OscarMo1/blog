afd8f1b30aca1d187279007a43d0cd7b8f20987f
OscarMo1 <111909746+OscarMo1@users.noreply.github.com>
2023-08-24 13:34:27 -0400

    Completed article info page
    styled to become responsive

--- src/pages/ArticleInfo/ArticleInfo.jsx
+++ src/pages/ArticleInfo/ArticleInfo.jsx
@@ -38,17 +38,27 @@ const postsData = [
   },
 ]
 
+const tagsData = [
+  "Medical",
+  "Lifestyle",
+  "Learn",
+  "Healthy",
+  "Food",
+  "Diet",
+  "Education",
+];
+
 const ArticleInfo = () => {
   return (
     <MainLayout>
-        <section className="container mx-auto max-w-5xl flex flex-col px-5 py-5">
+        <section className="container mx-auto max-w-5xl flex flex-col px-5 py-5 lg:flex-row lg:gap-x-5 lg:items-start">
             <article className="flex-1">
               <BreadCrumbs data={breadCrumbsData}/>
               <img className="rounded-xl w-full"src={images.Post1Image} alt="laptop" />
               <Link to="/blog?category=selectedCategory" className="text-primary text-sm font-roboto inline-block mt-4">
                 EDUCATION
               </Link>
-              <h1 className="text-xl font-medium font-roboto mt-4 text-dark-hard">
+              <h1 className="text-xl font-medium font-roboto mt-4 text-dark-hard md:text-[26px]">
                 Help children get better education
               </h1>
               <div className="mt-4 text-dark-soft">
@@ -59,7 +69,7 @@ const ArticleInfo = () => {
                 </p>
               </div>
             </article>
-            <SuggestedPosts header="Latest Article" posts={postsData}/>
+            <SuggestedPosts header="Latest Article" posts={postsData} tags={tagsData} className="mt-8 lg:mt-0 lg:max-w-xs"/>
         </section>
     </MainLayout>
   )
